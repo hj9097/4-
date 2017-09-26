aae2a37d29f5eb27d3bbfbd8376c2dae17f2f670
ParkTB <ppttbb9461@kookmin.ac.kr>
2017-09-26 10:59:39 +0900

    예외처리 수정

--- 20171626-박태범-assignment4.py
+++ 20171626-박태범-assignment4.py
@@ -5,23 +5,35 @@ def factorial(n):
             ans = ans*i
         return ans
 
-a = 0    #계속 루프를 돌기 위해 임의의 a 지정
+p = 0    #계속 루프를 돌기 위해 임의의 p 지정
 
-while a != -1:
-    n = int(input("Enter n : "))
-    if n == -1:    #n이 -1로 입력받으면 종료
-        break
-    a = factorial(n)
-    m = int(input("Enter m : "))
+while p != -1:
+    try:
+        n = int(input("Enter n : "))
+        if n == -1:    #n이 -1로 입력받으면 종료
+            break
+  
+        m = int(input("Enter m : "))
+    except:
+        print("error")
+        continue
 
     if n < 0 or m < 0:     #n이 -1이 아닌 음수 또는 m이 음수인 값을 입력받으면 에러메세지 출력
         print("올바른 값을 입력하세요")
         continue
 
-    if n == 0 and m != 0:  #n이 0이고 m은 0이 아닐때 에러메세지 출력
+    if n == 0 and m > 0:  #n이 0이고 m은 0이 아닐때 에러메세지 출력
+        print("0")
+        continue
+    if n == 0 and m < 0:
         print("Error")
         continue
 
+    if n < m:
+        print("Error")
+        continue
+
+    a = factorial(n)
     b = factorial(m)
     c = factorial(n-m)
 
